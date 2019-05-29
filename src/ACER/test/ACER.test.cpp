97f106e2ceab58970371d31dcf0ce993f365d506
Jeremy Lloyd Conlin <jlconlin@lanl.gov>
2019-05-29 14:45:42 -0600

    Adding some resources.

--- src/ACER/test/ACER.test.cpp
+++ src/ACER/test/ACER.test.cpp
@@ -4,4 +4,12 @@
 #include "ACER.hpp"
 
 SCENARIO( "Testing ACER" ){
-}
+  GIVEN( "ENDF and PENDF files" ){
+
+    decltype( auto ) sENDF = njoy::utility::slurpFileToMemory( "U235.endf" );
+    decltype( auto ) sPENDF = njoy::utility::slurpFileToMemory( "U235.pendf" );
+
+    decltype( auto ) ENDFtape = njoy::ENDFtk::syntaxTree::makeTape( sENDF );
+    decltype( auto ) PENDFtape = njoy::ENDFtk::syntaxTree::makeTape( sPENDF );
+  } // GIVEN
+} // SCENARIO
